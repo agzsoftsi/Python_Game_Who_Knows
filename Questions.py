124dc757b19b7fa8eeb635ef52421fa384c0519a
agzsoftsi <agzsoftsi@gmail.com>
2020-03-11 00:21:46 -0500

    Update Questions.py

--- Questions.py
+++ Questions.py
@@ -16,5 +16,9 @@ def Questions():
         print("""WHAT DOES THIS COMMAND LINE PRINT? \n>>> print("{:d} karlgarmor".format(32))""")
         print("""1 - 32 karlgarmor\n2 - karlgarmor 32\n3 - 32Karlgarmor\n4 - 32 Karlgarmor""")
         a = 1
+    elif q == 4:
+        print("""WHAT DOES THIS COMMAND LINE PRINT? \n>>> print("{:d} is, {}".format(1, "int"))""")
+        print("""1 - 1 is int\n2 - int is 1\n3 - 1 is, int\n4 - 1is,int""")
+        a = 3        
     else:
         print(q)
