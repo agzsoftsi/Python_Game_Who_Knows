389c22aec2d1243deb198d25ed5ded038be8e527
agzsoftsi <agzsoftsi@gmail.com>
2020-03-11 01:28:54 -0500

    Create Answers.py

---
+++ Answers.py
@@ -0,0 +1,41 @@
+"""Game Who Knows - Module of the All Answers """
+__author__ = "Carlos Andres Garcia Morales"
+
+from random import randint, uniform,random
+def Answers():
+ 
+    if q == 1:
+        
+        a = 1
+    elif q == 2:
+        
+        a = 3
+    elif q == 3:
+        
+        a = 1
+    elif q == 4:
+        
+        a = 3
+    elif q == 5:
+        
+        a = 3
+    elif q == 6:
+        
+        a = 4
+    elif q == 7:
+        
+        a = 2
+    elif q == 8:
+        
+        a = 1
+    elif q == 9:
+        
+        a = 4 
+    elif q == 10:
+        
+        a = 3      
+    else:
+        print(q)
+
+    return a
+
