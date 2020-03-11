e759e497045c470b9668fcfb0ead1a928c4594b2
agzsoftsi <agzsoftsi@gmail.com>
2020-03-10 23:53:28 -0500

    Create Modules

---
+++ GWKv1.py
@@ -0,0 +1,15 @@
+"""Game Who Knows - Module of the All Game """
+__author__ = "Carlos Andres Garcia Morales"
+print("WHO KNOWS")
+print("CHALLENGE QUESTIONS ABOUT PYTHON BASIC")
+print("LEVEL: DUMMIE")
+
+print("ONLY TYPE THE NUMBER OF THE ANSWER")
+
+score = 0
+
+from Questions import Questions
+
+print("----------------------------------------QUESTION 1-------------------------------------------")
+Questions()
+
---
+++ Questions.py
@@ -0,0 +1,16 @@
+"""Game Who Knows - Module of the All Questions """
+__author__ = "Carlos Andres Garcia Morales"
+
+from random import randint, uniform,random
+
+q = randint(1, 5)
+if q == 1:
+    print("WHO IS THE CREATOR OF PYTHON")
+    print("1 - Guido Van Rossum\n2 - Bill Gates\n3 - Julien Barbier\n4 - Richard Stallman")
+    a = 1
+elif q == 2:
+    print("""WHAT DOES THIS COMMAND LINE PRINT? >>> print("Easy Stuff")""")
+    print("""1 - easy Stuff\n2 - "Easy Stuff"\n3 - Easy Stuff\n4 - 'Easy Stuff'""")
+    a = 3
+else:
+    print(q)
