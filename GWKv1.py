693213fbf600c0e3d4cbbb5c1da6a0d7c1765d05
agzsoftsi <agzsoftsi@gmail.com>
2020-03-11 22:01:09 -0500

    Update GWKv1.py

--- GWKv1.py
+++ GWKv1.py
@@ -7,14 +7,38 @@ print("LEVEL: DUMMIE")
 print("ONLY TYPE THE NUMBER OF THE ANSWER")
 
 score = 0
-print("PLEASE ENTER YOUR NAME: ", end = "")
-name = str(input())
+# print("PLEASE ENTER YOUR NAME: ", end = "")
+# name = str(input())
 
-
-from Questions import Questions
+from colorama import init, Fore, Back, Style
+from Questions import QuestionsL1
 if __name__ == '__main__':
     print("----------------------------------------QUESTION 1-------------------------------------------")
-    Questions()
-    r = Questions()
-    print(r)
+    q1 = QuestionsL1()
+    
+    print("ENTER YOUR ANSWER: ", end = "")
+    r = int(input())
+
+    if q1[0] == 1 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 2 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 3 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 4 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 5 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 6 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 7 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 8 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 9 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 10 and q1[1] == r:
+       print("GOOD ANSWER")
+    else:
+        print("WRONG ANSWER")
 
