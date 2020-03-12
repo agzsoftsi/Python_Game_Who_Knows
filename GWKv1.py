a283235d1fb079954cc4bb3c2e23294b414a4f2d
agzsoftsi <agzsoftsi@gmail.com>
2020-03-11 23:48:59 -0500

    Update GWKv1.py

--- GWKv1.py
+++ GWKv1.py
@@ -41,3 +41,61 @@ if __name__ == '__main__':
     else:
         print("WRONG ANSWER")
 
+
+    print("----------------------------------------QUESTION 2-------------------------------------------")
+    q1 = QuestionsL1()
+    
+    print("ENTER YOUR ANSWER: ", end = "")
+    r = int(input())
+
+    if q1[0] == 1 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 2 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 3 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 4 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 5 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 6 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 7 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 8 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 9 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 10 and q1[1] == r:
+       print("GOOD ANSWER")
+    else:
+        print("WRONG ANSWER")
+
+    print("----------------------------------------QUESTION 3-------------------------------------------")
+    q1 = QuestionsL1()
+    
+    print("ENTER YOUR ANSWER: ", end = "")
+    r = int(input())
+
+    if q1[0] == 1 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 2 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 3 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 4 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 5 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 6 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 7 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 8 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 9 and q1[1] == r:
+       print("GOOD ANSWER")
+    elif q1[0] == 10 and q1[1] == r:
+       print("GOOD ANSWER")
+    else:
+        print("WRONG ANSWER")
