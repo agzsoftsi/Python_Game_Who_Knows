c226f127d088f921632a02f42c70d1ef23908687
agzsoftsi <agzsoftsi@gmail.com>
2020-03-12 23:45:21 +0000

    fix Pep8

--- GWKv1.py
+++ GWKv1.py
@@ -1,5 +1,6 @@
-"""Game Who Knows - Module of the All Game """
-__author__ = "Carlos Andres Garcia Morales"
+""" Game Who Knows - Module of the All Game """
+from Questions import QuestionsL1
+# __author__ = "Carlos Andres Garcia Morales"
 print("WHO KNOWS")
 print("CHALLENGE QUESTIONS ABOUT PYTHON BASIC")
 print("LEVEL: DUMMIES")
@@ -11,34 +12,31 @@ score_g = 0
 # print("PLEASE ENTER YOUR NAME: ", end = "")
 # name = str(input())
 
-from Questions import QuestionsL1
 if __name__ == '__main__':
     print("----------------------------------------QUESTION 1-------------------------------------------")
     q1 = QuestionsL1()
-    
-    print("ENTER YOUR ANSWER: ", end = "")
+    print("ENTER YOUR ANSWER: ", end="")
     r = int(input())
-
     if q1[0] == 1 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 2 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 3 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 4 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 5 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 6 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 7 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 8 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 9 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 10 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     else:
         print("WRONG ANSWER")
         score_g = 1
@@ -46,34 +44,31 @@ if __name__ == '__main__':
     if score_g == 0:
         score += 10
 
-
     print("----------------------------------------QUESTION 2-------------------------------------------")
     score_g = 0
     q1 = QuestionsL1()
-    
-    print("ENTER YOUR ANSWER: ", end = "")
+    print("ENTER YOUR ANSWER: ", end="")
     r = int(input())
-
     if q1[0] == 1 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 2 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 3 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 4 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 5 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 6 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 7 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 8 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 9 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 10 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     else:
         print("WRONG ANSWER")
         score_g = 1
@@ -84,30 +79,28 @@ if __name__ == '__main__':
     print("----------------------------------------QUESTION 3-------------------------------------------")
     score_g = 0
     q1 = QuestionsL1()
-    
-    print("ENTER YOUR ANSWER: ", end = "")
+    print("ENTER YOUR ANSWER: ", end="")
     r = int(input())
-
     if q1[0] == 1 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 2 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 3 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 4 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 5 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 6 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 7 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 8 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 9 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 10 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     else:
         print("WRONG ANSWER")
         score_g = 1
@@ -118,30 +111,28 @@ if __name__ == '__main__':
     print("----------------------------------------QUESTION 4-------------------------------------------")
     score_g = 0
     q1 = QuestionsL1()
-    
-    print("ENTER YOUR ANSWER: ", end = "")
+    print("ENTER YOUR ANSWER: ", end="")
     r = int(input())
-
     if q1[0] == 1 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 2 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 3 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 4 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 5 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 6 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 7 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 8 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 9 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 10 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     else:
         print("WRONG ANSWER")
         score_g = 1
@@ -152,30 +143,28 @@ if __name__ == '__main__':
     print("----------------------------------------QUESTION 5-------------------------------------------")
     score_g = 0
     q1 = QuestionsL1()
-    
-    print("ENTER YOUR ANSWER: ", end = "")
+    print("ENTER YOUR ANSWER: ", end="")
     r = int(input())
-
     if q1[0] == 1 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 2 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 3 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 4 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 5 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 6 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 7 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 8 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 9 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 10 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     else:
         print("WRONG ANSWER")
         score_g = 1
@@ -186,30 +175,28 @@ if __name__ == '__main__':
     print("----------------------------------------QUESTION 6-------------------------------------------")
     score_g = 0
     q1 = QuestionsL1()
-    
-    print("ENTER YOUR ANSWER: ", end = "")
+    print("ENTER YOUR ANSWER: ", end="")
     r = int(input())
-
     if q1[0] == 1 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 2 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 3 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 4 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 5 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 6 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 7 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 8 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 9 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 10 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     else:
         print("WRONG ANSWER")
         score_g = 1
@@ -220,143 +207,128 @@ if __name__ == '__main__':
     print("----------------------------------------QUESTION 7-------------------------------------------")
     score_g = 0
     q1 = QuestionsL1()
-    
-    print("ENTER YOUR ANSWER: ", end = "")
+    print("ENTER YOUR ANSWER: ", end="")
     r = int(input())
-
     if q1[0] == 1 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 2 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 3 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 4 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 5 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 6 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 7 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 8 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 9 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 10 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     else:
         print("WRONG ANSWER")
         score_g = 1
 
     if score_g == 0:
         score += 10
-
 
     print("----------------------------------------QUESTION 8-------------------------------------------")
     score_g = 0
     q1 = QuestionsL1()
-    
-    print("ENTER YOUR ANSWER: ", end = "")
+    print("ENTER YOUR ANSWER: ", end="")
     r = int(input())
-
     if q1[0] == 1 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 2 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 3 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 4 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 5 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 6 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 7 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 8 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 9 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 10 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     else:
         print("WRONG ANSWER")
         score_g = 1
 
     if score_g == 0:
         score += 10
-
-
 
     print("----------------------------------------QUESTION 9-------------------------------------------")
     score_g = 0
     q1 = QuestionsL1()
-    
-    print("ENTER YOUR ANSWER: ", end = "")
+    print("ENTER YOUR ANSWER: ", end="")
     r = int(input())
-
     if q1[0] == 1 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 2 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 3 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 4 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 5 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 6 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 7 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 8 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 9 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 10 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     else:
         print("WRONG ANSWER")
         score_g = 1
 
     if score_g == 0:
         score += 10
-
-
 
     print("----------------------------------------QUESTION 10-------------------------------------------")
     score_g = 0
     q1 = QuestionsL1()
-    
-    print("ENTER YOUR ANSWER: ", end = "")
+    print("ENTER YOUR ANSWER: ", end="")
     r = int(input())
-
     if q1[0] == 1 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 2 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 3 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 4 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 5 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 6 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 7 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 8 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 9 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     elif q1[0] == 10 and q1[1] == r:
-       print("GOOD ANSWER")
+        print("GOOD ANSWER")
     else:
         print("WRONG ANSWER")
         score_g = 1
 
     if score_g == 0:
         score += 10
-
-
     print("Your Score:  {:d}".format(score))
