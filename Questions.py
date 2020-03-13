bca8d1b1e21b75d3f74a2f361ffdfcba977dc350
agzsoftsi <agzsoftsi@gmail.com>
2020-03-12 22:21:13 -0500

    Update Questions.py

--- Questions.py
+++ Questions.py
@@ -98,3 +98,388 @@ def QuestionsL2():
         print(q)
 
     return (q,a)
+
+
+def QuestionsL3():
+    q = randint(1, 10)
+    if q == 1:
+        print("WHAT STAND FOR PIP?")
+        print("""1 - Python In Package\n2 - Package Installer for Python\n3 - Python Is Push\n4 - Package Inside Package""")
+        a = 2
+    elif q == 2:
+        print("WHAT STAND FOR PEP?")
+        print("""1 - Python Enhancement Proposal\n2 - Python Enter Package\n3 - Package Extra Python\n4 - Python Eight Path""")
+        a = 1
+    elif q == 3:
+        print("WHAT IS THE CORRECT WAY TO COMMENT MULTIPLE LINES OF CODE")
+        print("""1 - /* comment */\n2 - ### comment '''\n3 - --- comment ---\n4 - ''' comentario '''""")
+        a = 4
+    elif q == 4:
+        print("HOW DO YOU DEFINE A VARIABLE BY ASSIGNING IT A VALUE?")
+        print("""1 - number v = 0\n2 - var v = 0\n3 - int v = 0\n4 - v = 0""")
+        a = 4
+    elif q == 5:
+        print("""WHAT DOES THIS COMMAND LINE PRINT? \n>>> print('%.2f' % 1714.666)""")
+        print("""1 - 1714.67\n2 - 1714.0\n3 - 1714.66\n4 - 1715""")
+        a = 1
+    elif q == 6:
+        print("WHICH OF THE FOLLOWING IS NOT A VALID ASSIGNMENT OPERATOR?")
+        print("""1 - |=1\n2 - @=1\n3 - //=1\n4 - +=1""")
+        a = 2
+    elif q == 7:
+        print("WHAT IS PYTHON?")
+        print("""1 - A low-level, non-object-oriented compiled programming language\n2 - A machine language executed in a java virtual machine\n3 - A high-level object-oriented interpreted programming language\n4 - A programming language invented by the creator of Linux""")
+        a = 3
+    elif q == 8:
+        print("HOW TO KNOW THE TYPE OF A CERTAIN VARIABLE?")
+        print("""1 - TypeOf(1)\n2 - type(1)\n3 - is_a?\n4 - typeof 1""")
+        a = 2
+    elif q == 9:
+        print("TRANSFORM A COMPATIBLE OBJECT INTO A CHARACTER STRING")
+        print("""1 - str()\n2 - int()\n3 - char()\n4 - transform()""")
+        a = 1 
+    elif q == 10:
+        print("THAT DATA TYPE IS NOT INTEGER")
+        print("""1 - 0x18\n2 - 0b010011\n3 - 12.\n4 - 60""")
+        a = 3      
+    else:
+        print(q)
+
+    return (q,a)
+
+
+def QuestionsL4():
+    q = randint(1, 10)
+    if q == 1:
+        print("WHAT STAND FOR PIP?")
+        print("""1 - Python In Package\n2 - Package Installer for Python\n3 - Python Is Push\n4 - Package Inside Package""")
+        a = 2
+    elif q == 2:
+        print("WHAT STAND FOR PEP?")
+        print("""1 - Python Enhancement Proposal\n2 - Python Enter Package\n3 - Package Extra Python\n4 - Python Eight Path""")
+        a = 1
+    elif q == 3:
+        print("WHAT IS THE CORRECT WAY TO COMMENT MULTIPLE LINES OF CODE")
+        print("""1 - /* comment */\n2 - ### comment '''\n3 - --- comment ---\n4 - ''' comentario '''""")
+        a = 4
+    elif q == 4:
+        print("HOW DO YOU DEFINE A VARIABLE BY ASSIGNING IT A VALUE?")
+        print("""1 - number v = 0\n2 - var v = 0\n3 - int v = 0\n4 - v = 0""")
+        a = 4
+    elif q == 5:
+        print("""WHAT DOES THIS COMMAND LINE PRINT? \n>>> print('%.2f' % 1714.666)""")
+        print("""1 - 1714.67\n2 - 1714.0\n3 - 1714.66\n4 - 1715""")
+        a = 1
+    elif q == 6:
+        print("WHICH OF THE FOLLOWING IS NOT A VALID ASSIGNMENT OPERATOR?")
+        print("""1 - |=1\n2 - @=1\n3 - //=1\n4 - +=1""")
+        a = 2
+    elif q == 7:
+        print("WHAT IS PYTHON?")
+        print("""1 - A low-level, non-object-oriented compiled programming language\n2 - A machine language executed in a java virtual machine\n3 - A high-level object-oriented interpreted programming language\n4 - A programming language invented by the creator of Linux""")
+        a = 3
+    elif q == 8:
+        print("HOW TO KNOW THE TYPE OF A CERTAIN VARIABLE?")
+        print("""1 - TypeOf(1)\n2 - type(1)\n3 - is_a?\n4 - typeof 1""")
+        a = 2
+    elif q == 9:
+        print("TRANSFORM A COMPATIBLE OBJECT INTO A CHARACTER STRING")
+        print("""1 - str()\n2 - int()\n3 - char()\n4 - transform()""")
+        a = 1 
+    elif q == 10:
+        print("THAT DATA TYPE IS NOT INTEGER")
+        print("""1 - 0x18\n2 - 0b010011\n3 - 12.\n4 - 60""")
+        a = 3      
+    else:
+        print(q)
+
+    return (q,a)
+
+
+
+def QuestionsL5():
+    q = randint(1, 10)
+    if q == 1:
+        print("WHAT STAND FOR PIP?")
+        print("""1 - Python In Package\n2 - Package Installer for Python\n3 - Python Is Push\n4 - Package Inside Package""")
+        a = 2
+    elif q == 2:
+        print("WHAT STAND FOR PEP?")
+        print("""1 - Python Enhancement Proposal\n2 - Python Enter Package\n3 - Package Extra Python\n4 - Python Eight Path""")
+        a = 1
+    elif q == 3:
+        print("WHAT IS THE CORRECT WAY TO COMMENT MULTIPLE LINES OF CODE")
+        print("""1 - /* comment */\n2 - ### comment '''\n3 - --- comment ---\n4 - ''' comentario '''""")
+        a = 4
+    elif q == 4:
+        print("HOW DO YOU DEFINE A VARIABLE BY ASSIGNING IT A VALUE?")
+        print("""1 - number v = 0\n2 - var v = 0\n3 - int v = 0\n4 - v = 0""")
+        a = 4
+    elif q == 5:
+        print("""WHAT DOES THIS COMMAND LINE PRINT? \n>>> print('%.2f' % 1714.666)""")
+        print("""1 - 1714.67\n2 - 1714.0\n3 - 1714.66\n4 - 1715""")
+        a = 1
+    elif q == 6:
+        print("WHICH OF THE FOLLOWING IS NOT A VALID ASSIGNMENT OPERATOR?")
+        print("""1 - |=1\n2 - @=1\n3 - //=1\n4 - +=1""")
+        a = 2
+    elif q == 7:
+        print("WHAT IS PYTHON?")
+        print("""1 - A low-level, non-object-oriented compiled programming language\n2 - A machine language executed in a java virtual machine\n3 - A high-level object-oriented interpreted programming language\n4 - A programming language invented by the creator of Linux""")
+        a = 3
+    elif q == 8:
+        print("HOW TO KNOW THE TYPE OF A CERTAIN VARIABLE?")
+        print("""1 - TypeOf(1)\n2 - type(1)\n3 - is_a?\n4 - typeof 1""")
+        a = 2
+    elif q == 9:
+        print("TRANSFORM A COMPATIBLE OBJECT INTO A CHARACTER STRING")
+        print("""1 - str()\n2 - int()\n3 - char()\n4 - transform()""")
+        a = 1 
+    elif q == 10:
+        print("THAT DATA TYPE IS NOT INTEGER")
+        print("""1 - 0x18\n2 - 0b010011\n3 - 12.\n4 - 60""")
+        a = 3      
+    else:
+        print(q)
+
+    return (q,a)
+
+
+
+def QuestionsL6():
+    q = randint(1, 10)
+    if q == 1:
+        print("WHAT STAND FOR PIP?")
+        print("""1 - Python In Package\n2 - Package Installer for Python\n3 - Python Is Push\n4 - Package Inside Package""")
+        a = 2
+    elif q == 2:
+        print("WHAT STAND FOR PEP?")
+        print("""1 - Python Enhancement Proposal\n2 - Python Enter Package\n3 - Package Extra Python\n4 - Python Eight Path""")
+        a = 1
+    elif q == 3:
+        print("WHAT IS THE CORRECT WAY TO COMMENT MULTIPLE LINES OF CODE")
+        print("""1 - /* comment */\n2 - ### comment '''\n3 - --- comment ---\n4 - ''' comentario '''""")
+        a = 4
+    elif q == 4:
+        print("HOW DO YOU DEFINE A VARIABLE BY ASSIGNING IT A VALUE?")
+        print("""1 - number v = 0\n2 - var v = 0\n3 - int v = 0\n4 - v = 0""")
+        a = 4
+    elif q == 5:
+        print("""WHAT DOES THIS COMMAND LINE PRINT? \n>>> print('%.2f' % 1714.666)""")
+        print("""1 - 1714.67\n2 - 1714.0\n3 - 1714.66\n4 - 1715""")
+        a = 1
+    elif q == 6:
+        print("WHICH OF THE FOLLOWING IS NOT A VALID ASSIGNMENT OPERATOR?")
+        print("""1 - |=1\n2 - @=1\n3 - //=1\n4 - +=1""")
+        a = 2
+    elif q == 7:
+        print("WHAT IS PYTHON?")
+        print("""1 - A low-level, non-object-oriented compiled programming language\n2 - A machine language executed in a java virtual machine\n3 - A high-level object-oriented interpreted programming language\n4 - A programming language invented by the creator of Linux""")
+        a = 3
+    elif q == 8:
+        print("HOW TO KNOW THE TYPE OF A CERTAIN VARIABLE?")
+        print("""1 - TypeOf(1)\n2 - type(1)\n3 - is_a?\n4 - typeof 1""")
+        a = 2
+    elif q == 9:
+        print("TRANSFORM A COMPATIBLE OBJECT INTO A CHARACTER STRING")
+        print("""1 - str()\n2 - int()\n3 - char()\n4 - transform()""")
+        a = 1 
+    elif q == 10:
+        print("THAT DATA TYPE IS NOT INTEGER")
+        print("""1 - 0x18\n2 - 0b010011\n3 - 12.\n4 - 60""")
+        a = 3      
+    else:
+        print(q)
+
+    return (q,a)
+
+
+def QuestionsL7():
+    q = randint(1, 10)
+    if q == 1:
+        print("WHAT STAND FOR PIP?")
+        print("""1 - Python In Package\n2 - Package Installer for Python\n3 - Python Is Push\n4 - Package Inside Package""")
+        a = 2
+    elif q == 2:
+        print("WHAT STAND FOR PEP?")
+        print("""1 - Python Enhancement Proposal\n2 - Python Enter Package\n3 - Package Extra Python\n4 - Python Eight Path""")
+        a = 1
+    elif q == 3:
+        print("WHAT IS THE CORRECT WAY TO COMMENT MULTIPLE LINES OF CODE")
+        print("""1 - /* comment */\n2 - ### comment '''\n3 - --- comment ---\n4 - ''' comentario '''""")
+        a = 4
+    elif q == 4:
+        print("HOW DO YOU DEFINE A VARIABLE BY ASSIGNING IT A VALUE?")
+        print("""1 - number v = 0\n2 - var v = 0\n3 - int v = 0\n4 - v = 0""")
+        a = 4
+    elif q == 5:
+        print("""WHAT DOES THIS COMMAND LINE PRINT? \n>>> print('%.2f' % 1714.666)""")
+        print("""1 - 1714.67\n2 - 1714.0\n3 - 1714.66\n4 - 1715""")
+        a = 1
+    elif q == 6:
+        print("WHICH OF THE FOLLOWING IS NOT A VALID ASSIGNMENT OPERATOR?")
+        print("""1 - |=1\n2 - @=1\n3 - //=1\n4 - +=1""")
+        a = 2
+    elif q == 7:
+        print("WHAT IS PYTHON?")
+        print("""1 - A low-level, non-object-oriented compiled programming language\n2 - A machine language executed in a java virtual machine\n3 - A high-level object-oriented interpreted programming language\n4 - A programming language invented by the creator of Linux""")
+        a = 3
+    elif q == 8:
+        print("HOW TO KNOW THE TYPE OF A CERTAIN VARIABLE?")
+        print("""1 - TypeOf(1)\n2 - type(1)\n3 - is_a?\n4 - typeof 1""")
+        a = 2
+    elif q == 9:
+        print("TRANSFORM A COMPATIBLE OBJECT INTO A CHARACTER STRING")
+        print("""1 - str()\n2 - int()\n3 - char()\n4 - transform()""")
+        a = 1 
+    elif q == 10:
+        print("THAT DATA TYPE IS NOT INTEGER")
+        print("""1 - 0x18\n2 - 0b010011\n3 - 12.\n4 - 60""")
+        a = 3      
+    else:
+        print(q)
+
+    return (q,a)
+
+
+def QuestionsL8():
+    q = randint(1, 10)
+    if q == 1:
+        print("WHAT STAND FOR PIP?")
+        print("""1 - Python In Package\n2 - Package Installer for Python\n3 - Python Is Push\n4 - Package Inside Package""")
+        a = 2
+    elif q == 2:
+        print("WHAT STAND FOR PEP?")
+        print("""1 - Python Enhancement Proposal\n2 - Python Enter Package\n3 - Package Extra Python\n4 - Python Eight Path""")
+        a = 1
+    elif q == 3:
+        print("WHAT IS THE CORRECT WAY TO COMMENT MULTIPLE LINES OF CODE")
+        print("""1 - /* comment */\n2 - ### comment '''\n3 - --- comment ---\n4 - ''' comentario '''""")
+        a = 4
+    elif q == 4:
+        print("HOW DO YOU DEFINE A VARIABLE BY ASSIGNING IT A VALUE?")
+        print("""1 - number v = 0\n2 - var v = 0\n3 - int v = 0\n4 - v = 0""")
+        a = 4
+    elif q == 5:
+        print("""WHAT DOES THIS COMMAND LINE PRINT? \n>>> print('%.2f' % 1714.666)""")
+        print("""1 - 1714.67\n2 - 1714.0\n3 - 1714.66\n4 - 1715""")
+        a = 1
+    elif q == 6:
+        print("WHICH OF THE FOLLOWING IS NOT A VALID ASSIGNMENT OPERATOR?")
+        print("""1 - |=1\n2 - @=1\n3 - //=1\n4 - +=1""")
+        a = 2
+    elif q == 7:
+        print("WHAT IS PYTHON?")
+        print("""1 - A low-level, non-object-oriented compiled programming language\n2 - A machine language executed in a java virtual machine\n3 - A high-level object-oriented interpreted programming language\n4 - A programming language invented by the creator of Linux""")
+        a = 3
+    elif q == 8:
+        print("HOW TO KNOW THE TYPE OF A CERTAIN VARIABLE?")
+        print("""1 - TypeOf(1)\n2 - type(1)\n3 - is_a?\n4 - typeof 1""")
+        a = 2
+    elif q == 9:
+        print("TRANSFORM A COMPATIBLE OBJECT INTO A CHARACTER STRING")
+        print("""1 - str()\n2 - int()\n3 - char()\n4 - transform()""")
+        a = 1 
+    elif q == 10:
+        print("THAT DATA TYPE IS NOT INTEGER")
+        print("""1 - 0x18\n2 - 0b010011\n3 - 12.\n4 - 60""")
+        a = 3      
+    else:
+        print(q)
+
+    return (q,a)
+
+def QuestionsL9():
+    q = randint(1, 10)
+    if q == 1:
+        print("WHAT STAND FOR PIP?")
+        print("""1 - Python In Package\n2 - Package Installer for Python\n3 - Python Is Push\n4 - Package Inside Package""")
+        a = 2
+    elif q == 2:
+        print("WHAT STAND FOR PEP?")
+        print("""1 - Python Enhancement Proposal\n2 - Python Enter Package\n3 - Package Extra Python\n4 - Python Eight Path""")
+        a = 1
+    elif q == 3:
+        print("WHAT IS THE CORRECT WAY TO COMMENT MULTIPLE LINES OF CODE")
+        print("""1 - /* comment */\n2 - ### comment '''\n3 - --- comment ---\n4 - ''' comentario '''""")
+        a = 4
+    elif q == 4:
+        print("HOW DO YOU DEFINE A VARIABLE BY ASSIGNING IT A VALUE?")
+        print("""1 - number v = 0\n2 - var v = 0\n3 - int v = 0\n4 - v = 0""")
+        a = 4
+    elif q == 5:
+        print("""WHAT DOES THIS COMMAND LINE PRINT? \n>>> print('%.2f' % 1714.666)""")
+        print("""1 - 1714.67\n2 - 1714.0\n3 - 1714.66\n4 - 1715""")
+        a = 1
+    elif q == 6:
+        print("WHICH OF THE FOLLOWING IS NOT A VALID ASSIGNMENT OPERATOR?")
+        print("""1 - |=1\n2 - @=1\n3 - //=1\n4 - +=1""")
+        a = 2
+    elif q == 7:
+        print("WHAT IS PYTHON?")
+        print("""1 - A low-level, non-object-oriented compiled programming language\n2 - A machine language executed in a java virtual machine\n3 - A high-level object-oriented interpreted programming language\n4 - A programming language invented by the creator of Linux""")
+        a = 3
+    elif q == 8:
+        print("HOW TO KNOW THE TYPE OF A CERTAIN VARIABLE?")
+        print("""1 - TypeOf(1)\n2 - type(1)\n3 - is_a?\n4 - typeof 1""")
+        a = 2
+    elif q == 9:
+        print("TRANSFORM A COMPATIBLE OBJECT INTO A CHARACTER STRING")
+        print("""1 - str()\n2 - int()\n3 - char()\n4 - transform()""")
+        a = 1 
+    elif q == 10:
+        print("THAT DATA TYPE IS NOT INTEGER")
+        print("""1 - 0x18\n2 - 0b010011\n3 - 12.\n4 - 60""")
+        a = 3      
+    else:
+        print(q)
+
+    return (q,a)
+
+
+def QuestionsL10():
+    q = randint(1, 10)
+    if q == 1:
+        print("WHAT STAND FOR PIP?")
+        print("""1 - Python In Package\n2 - Package Installer for Python\n3 - Python Is Push\n4 - Package Inside Package""")
+        a = 2
+    elif q == 2:
+        print("WHAT STAND FOR PEP?")
+        print("""1 - Python Enhancement Proposal\n2 - Python Enter Package\n3 - Package Extra Python\n4 - Python Eight Path""")
+        a = 1
+    elif q == 3:
+        print("WHAT IS THE CORRECT WAY TO COMMENT MULTIPLE LINES OF CODE")
+        print("""1 - /* comment */\n2 - ### comment '''\n3 - --- comment ---\n4 - ''' comentario '''""")
+        a = 4
+    elif q == 4:
+        print("HOW DO YOU DEFINE A VARIABLE BY ASSIGNING IT A VALUE?")
+        print("""1 - number v = 0\n2 - var v = 0\n3 - int v = 0\n4 - v = 0""")
+        a = 4
+    elif q == 5:
+        print("""WHAT DOES THIS COMMAND LINE PRINT? \n>>> print('%.2f' % 1714.666)""")
+        print("""1 - 1714.67\n2 - 1714.0\n3 - 1714.66\n4 - 1715""")
+        a = 1
+    elif q == 6:
+        print("WHICH OF THE FOLLOWING IS NOT A VALID ASSIGNMENT OPERATOR?")
+        print("""1 - |=1\n2 - @=1\n3 - //=1\n4 - +=1""")
+        a = 2
+    elif q == 7:
+        print("WHAT IS PYTHON?")
+        print("""1 - A low-level, non-object-oriented compiled programming language\n2 - A machine language executed in a java virtual machine\n3 - A high-level object-oriented interpreted programming language\n4 - A programming language invented by the creator of Linux""")
+        a = 3
+    elif q == 8:
+        print("HOW TO KNOW THE TYPE OF A CERTAIN VARIABLE?")
+        print("""1 - TypeOf(1)\n2 - type(1)\n3 - is_a?\n4 - typeof 1""")
+        a = 2
+    elif q == 9:
+        print("TRANSFORM A COMPATIBLE OBJECT INTO A CHARACTER STRING")
+        print("""1 - str()\n2 - int()\n3 - char()\n4 - transform()""")
+        a = 1 
+    elif q == 10:
+        print("THAT DATA TYPE IS NOT INTEGER")
+        print("""1 - 0x18\n2 - 0b010011\n3 - 12.\n4 - 60""")
+        a = 3      
+    else:
+        print(q)
+
+    return (q,a)
